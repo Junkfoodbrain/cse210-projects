a43d066da8a6237a83efdfc6ef7594b1cb62b29f
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-03 00:16:37 -0700

    Part 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,26 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        int guess = -1;
+
 
         while (guess != magicNumber)
         {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
             if (guess < magicNumber)
             {
                 Console.WriteLine("Higher");
             }
-            else
+            else if (magicNumber < guess)
             {
                 Console.WriteLine("Lower");
             }
-
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            
         }
-        Console.WriteLine("You guessed it!");
+        
     }
 }
