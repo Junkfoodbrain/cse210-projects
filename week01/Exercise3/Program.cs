9dc4d494dc3407fe52595eaa0fd43ce44597f9c4
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-03 00:09:30 -0700

    Step 2 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -9,19 +9,21 @@ class Program
 
         Console.Write("What is your guess? ");
         int guess = int.Parse(Console.ReadLine());
-        
-        
-        if (guess < magicNumber)
+
+        while (guess != magicNumber)
         {
-            Console.WriteLine("Higher");
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
         }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+        Console.WriteLine("You guessed it!");
     }
 }
