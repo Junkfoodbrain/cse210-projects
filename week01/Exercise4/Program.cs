fb03e7a329211289829ad9cdbd1e28f33f87a3fd
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-06 10:18:10 -0700

    Part 3 complete: Find the max

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -32,12 +32,15 @@ class Program
         int max = numbers[0];
         foreach (int number in numbers)
         {
-            
+           if (number > max)
+            {
+                max = number;
+            } 
         }
 
         Console.WriteLine($"The sum is: {sum}");
-
         Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
     }
     
 }
