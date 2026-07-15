eea8a59ab535b844cf823009c220e50c0807713d
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-14 21:53:29 -0700

    completed menu options and tested. will do some minor changes but all is working.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -24,33 +24,52 @@ class Program
             string choice = Console.ReadLine();
             Console.WriteLine();
 
+            //write
             if (choice == "1")
             {
-
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToString("yyyy-MM-dd");
+                entry._promptText = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"Prompt: {entry._promptText}");
+                Console.Write("Your response: ");
+                entry._entryText = Console.ReadLine();
+                theJournal.AddEntry(entry);
+                Console.WriteLine("Entry added.");
             }
+            //display
             else if (choice == "2")
             {
-
+                theJournal.DisplayAll();
             }
+            //load
             else if (choice == "3")
             {
-
+                Console.Write("Filename to load: ");
+                string loadFile = Console.ReadLine();
+                theJournal.LoadFromFile(loadFile);
+                Console.WriteLine("Journal loaded. ");
             }
+            //save
             else if (choice == "4")
             {
-
+                Console.Write("Filename to save: ");
+                string saveFile = Console.ReadLine();
+                theJournal.SaveToFile(saveFile);
+                Console.WriteLine("Journal saved.");
             }
+            //end loop/quit
             else if (choice == "5")
             {
                 running = false;
             }
+            //if anything other than the given options is entered. Prompts user to try again.
             else
             {
                 Console.WriteLine("Please try again, using an option from the list.");
             }
 
-        }
-
-        Console.WriteLine();
+            //write a blank line before the menu repeats. Gives the user a visual break.
+            Console.WriteLine();
+        }        
     }
 }
