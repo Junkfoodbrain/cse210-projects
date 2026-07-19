7ee90cc3fb9273943eb856dff3c591342b144f90
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-18 17:19:44 -0700

    finished through step 6. rearranged items so they grouped together and printed out in order.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,17 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(6);
-        Fraction fraction3 = new Fraction(6, 7);
-
-
         Console.WriteLine(fraction1.GetFractionString());
-        Console.WriteLine(fraction2.GetFractionString());
-        Console.WriteLine(fraction3.GetFractionString());
-
         Console.WriteLine(fraction1.GetDecimalValue());
+
+
+        Fraction fraction2 = new Fraction(1, 1);
+        Console.WriteLine(fraction2.GetFractionString());
         Console.WriteLine(fraction2.GetDecimalValue());
+
+
+        Fraction fraction3 = new Fraction(5, 1);       
+        Console.WriteLine(fraction3.GetFractionString());
         Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(3, 4);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
+        Fraction fraction5 = new Fraction(1, 3);
+        Console.WriteLine(fraction5.GetFractionString());
+        Console.WriteLine(fraction5.GetDecimalValue());
 
     }
 }
