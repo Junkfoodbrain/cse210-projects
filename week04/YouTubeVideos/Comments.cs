f4d9be497096838b11fe47f585411784c3ce1f47
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-27 23:50:03 -0700

    just need to save everything here.... almost done

--- week04/YouTubeVideos/Comments.cs
+++ week04/YouTubeVideos/Comments.cs
@@ -3,12 +3,27 @@ public class Comments
     private string _name;
     private string _text;
 
-    public void getName()
+    public Comments(string name, string text)
     {
-        
+        _name = name;
+        _text = text;
     }
-    public void getText()
+
+    public string getName()
     {
-        
+        return _name;
+    }
+    public string getText()
+    {
+        return _text;
+    }
+
+    public void setName(string name)
+    {
+        _name = name;
+    }
+    public void setText(string text)
+    {
+        _text = text;
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 public class Video
 {
     private string _title;
@@ -5,36 +7,44 @@ public class Video
     private int _length;
     private List<Comment> _comments;
 
+    public Video(string title, string author, int Length)
+    {
+        _title = title;
+        _author = author;
+        _length = Length;
+        _comments = new List<Comment>();
+    }
+
     public string getTitle()
     {
-
+        return _title;
     }
     public string getAuthor()
     {
-
+        return _author;
     }
     public int getLength()
     {
-
+        return _length;
     }
     public void addComment(Comment comment)
     {
-
+        _comments.Add(comment);
     }
     public int getNumberOfComments()
     {
-
+        return _comments.Count;
     }
     public void setTitle(string title)
     {
-
+        _title = title;
     }
     public void setAuthor(string author)
     {
-
+        _author = author;
     }
     public void setLength(int length)
     {
-        
+        _length = length;
     }
 }
