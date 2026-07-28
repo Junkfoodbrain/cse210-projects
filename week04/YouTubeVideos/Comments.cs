083f5c630d7316b539bbc5faff4eeef1d19a8cf3
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-27 20:30:46 -0700

    initial push to stub out YouTube Video Program

---
+++ week04/YouTubeVideos/Comments.cs
@@ -0,0 +1,14 @@
+public class Comments
+{
+    private string _name;
+    private string _text;
+
+    public void getName()
+    {
+        
+    }
+    public void getText()
+    {
+        
+    }
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,40 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments;
+
+    public string getTitle()
+    {
+
+    }
+    public string getAuthor()
+    {
+
+    }
+    public int getLength()
+    {
+
+    }
+    public void addComment(Comment comment)
+    {
+
+    }
+    public int getNumberOfComments()
+    {
+
+    }
+    public void setTitle(string title)
+    {
+
+    }
+    public void setAuthor(string author)
+    {
+
+    }
+    public void setLength(int length)
+    {
+        
+    }
+}
