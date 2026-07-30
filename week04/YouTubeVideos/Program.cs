fb7dd9ac57aa3983ab87e0b795168df5d0a92fb0
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-07-29 21:53:14 -0700

    updated to iterate through list of video objects and display the info and comments with details.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -10,75 +11,51 @@ class Program
 
         Comment comment1 = new Comment("Tim", "Great catch!");
         video1.addComment(comment1);
-
         Comment comment2 = new Comment("Jeff", "I caught a big one with your technique!");
         video1.addComment(comment2);
-
         Comment comment3 = new Comment("Jessie", "Now we'll never go home hungry!");
         video1.addComment(comment3);
-
         Comment comment4 = new Comment("Troy", "No more 'fish tales' now I'm bringing home the big ones!");
         video1.addComment(comment4);
-
-        Console.WriteLine("Title: " + video1.getTitle());
-        Console.WriteLine("Author: " + video1.getAuthor());
-        Console.WriteLine("Length: " + video1.getLength() + " seconds");
-        Console.WriteLine("Number of Comments: " + video1.getNumberOfComments());
-        Console.WriteLine("Comments: ");
-
-        foreach (Comment comment in video1.getComments())
-        {
-            Console.WriteLine(comment.getName() + ": " + comment.getText());
-        }
-        Console.WriteLine();
 
 
         Video video2 = new Video("Gardening with Clara", "Clara Green", 450);
 
         Comment comment5 = new Comment("Jenny", "Love your gardening tips!");
         video2.addComment(comment5);
-
         Comment comment6 = new Comment("Mark", "You thought of everything in the raised bed plans!");
         video2.addComment(comment6);
-
         Comment comment7 = new Comment("Terry", "My garden is already thriving after making the changes from your video.");
         video2.addComment(comment7);
-
-        Console.WriteLine("Title: " + video2.getTitle());
-        Console.WriteLine("Author: " + video2.getAuthor());
-        Console.WriteLine("Length: " + video2.getLength() + " seconds");
-        Console.WriteLine("Number of Comments: " + video2.getNumberOfComments());
-        Console.WriteLine("Comments: ");
-
-        foreach (Comment comment in video2.getComments())
-        {
-            Console.WriteLine(comment.getName() + ": " + comment.getText());
-        }
-        Console.WriteLine();
-
+        
 
         Video video3 = new Video("Baking for Dummies", "Becky Baker", 600);
 
         Comment comment8 = new Comment("Stephanie", "Your recipes are so easy to follow!");
         video3.addComment(comment8);
-
         Comment comment9 = new Comment("Michael", "I made my daughter's wedding cake using your recipe!");
         video3.addComment(comment9);
-
         Comment comment10 = new Comment("Leah", "Wow, it turned out amazing!");
         video3.addComment(comment10);
+       
 
-        Console.WriteLine("Title: " + video3.getTitle());
-        Console.WriteLine("Author: " + video3.getAuthor());
-        Console.WriteLine("Length: " + video3.getLength() + " seconds");
-        Console.WriteLine("Number of Comments: " + video3.getNumberOfComments());
-        Console.WriteLine("Comments: ");
+        List<Video> videos = new List<Video> { video1, video2, video3 }; 
 
-        foreach (Comment comment in video3.getComments())
+        foreach (Video video in videos)
         {
-            Console.WriteLine(comment.getName() + ": " + comment.getText());
+            Console.WriteLine("Title: " + video.getTitle());
+            Console.WriteLine("Author: " + video.getAuthor());
+            Console.WriteLine("Length: " + video.getLength() + " seconds");
+            Console.WriteLine("Number of Comments: " + video.getNumberOfComments());
+            Console.WriteLine("Comments:");
+
+            foreach (Comment comment in video.getComments())
+            {
+                Console.WriteLine(comment.getName() + ": " + comment.getText());
+            }
+
+            Console.WriteLine();
         }
-        Console.WriteLine();
         
     }
 }
