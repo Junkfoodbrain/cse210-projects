7b310b8b126475937dc956403f6b7f69c6620fbf
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-01 11:17:57 -0700

    pushing learning activity to github

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("James Kirk", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Stuart Little", "Fractions", "1.3", "7-9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Ron Weasley", "Accidental Magic", "A Journey into Sweet Surprises");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
+        
     }
 }
