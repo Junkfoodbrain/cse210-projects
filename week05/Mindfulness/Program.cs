e115c1b193d33c1471e669193d6323a0eabffcf4
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-03 23:05:43 -0700

    fixing so "quit" or "Quit" works.

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -9,7 +9,7 @@ class Program
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
 
         string choice = "";
-        while (choice != "4")
+        while (choice != "4" && !string.Equals(choice, "quit", StringComparison.OrdinalIgnoreCase))
         {
             Console.Clear();
             Console.WriteLine("Menu Options:");
