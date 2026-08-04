dfbcfd568346a4dcbaa30b58cdd24f261a309f59
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-03 23:11:34 -0700

    updating hello message.

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("Hello! Welcome to Leah Sorensen's Mindfulness Project.");
 
         string choice = "";
         while (choice != "4")
