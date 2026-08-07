8a44f710321893d78bd7863af5768166023ca1e0
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-06 23:13:15 -0700

    initial push, basic stub out...

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Blue", 3, 5);
+        Circle circle = new Circle("Green", 2);
+
+
+        Console.WriteLine($"Square: color={square.GetColor()}, Area={square.GetArea()}");
+        Console.WriteLine($"Rectangle: color={rectangle.GetColor()}, Area={rectangle.GetArea()}");
+        Console.WriteLine($"Circle: Color={circle.GetColor()}, Area={circle.GetArea()}");
     }
 }
