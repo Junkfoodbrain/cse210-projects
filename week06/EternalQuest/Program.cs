9e70180eb2dc8071e8bf26eac0bb627b993dd8b9
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-07 23:15:45 -0700

    setting up GoalManager... not complete.

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        GoalManager manager = new GoalManager();
+        manager.Start();
+
+        
         Console.WriteLine("Hello World! This is the EternalQuest Project.");
     }
 }
