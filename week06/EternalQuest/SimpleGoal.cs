67177a75d5901dca6cf1059b8374e5d0f697c4e2
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-07 22:19:10 -0700

    Initial push, beginning stub out

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override int RecordEvent()
+    {
+        return 0;
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
