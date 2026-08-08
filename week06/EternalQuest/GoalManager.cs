7893fcc26136e9601ae1f82bc03459935a1c54ce
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-08-07 23:47:20 -0700

    added basic menu per example.. may change things later.

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -13,7 +13,30 @@ public class GoalManager
 
     public void Start()
     {
-        
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine($"You currently have {_score} points.");
+            Console.WriteLine();
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine(" 1. Create New Goal");
+            Console.WriteLine(" 2. List Goals");
+            Console.WriteLine(" 3. Save Goals");
+            Console.WriteLine(" 4. Load Goals");
+            Console.WriteLine(" 5. Record Event");
+            Console.WriteLine(" 6. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+            if (choice == "6")
+            {
+                isRunning = false;
+            }
+
+        }
     }
     
 }
